c1ad08ba450c82250d539eff78c417226013807f
thePythoninja <olaoluwathompson8@gmail.com>
2024-07-10 20:47:24 +0100

    upgrades

--- script.js
+++ script.js
@@ -28,8 +28,12 @@ let ticketNumbers = "";
 })();
 
 let reset = false;
+const duration = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 async function Lottery() {
-  for (let i = 0; i < 30; i++) {
+  const durationLength = Math.floor(Math.random() * duration.length) + 1;
+
+  for (let i = 0; i < duration[durationLength]; i++) {
+    console.log(duration[durationLength]);
     // Generate a random number from 0 to length of arr
     const randNum = Math.floor(Math.random() * ticketNumbers.length);
     // Add a one second delay
